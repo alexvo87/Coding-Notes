f9960030edd92ea7ea78709472edf64366fa026e
vtatntpt <anhtuanadmin@gmail.com>
2017-03-25 09:35:55 +0700

    Update Shared Preferences

--- Android/SharedPreference.java
+++ Android/SharedPreference.java
@@ -122,5 +122,167 @@ private void addInJSONArray(Product productToAdd){
 
 //############################################################################
 
+package com.example;
+// Change the package name to match your package
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class ArrayHelper {
+  Context context;
+    SharedPreferences prefs;
+    SharedPreferences.Editor editor;
+    
+    public ArrayHelper(Context context) {
+        this.context = context;
+        prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        editor = prefs.edit();
+    }
+    
+    /**
+     * Converts the provided ArrayList<String>
+     * into a JSONArray and saves it as a single
+     * string in the apps shared preferences
+     * @param String key Preference key for SharedPreferences
+     * @param array ArrayList<String> containing the list items
+     */
+    public void saveArray(String key, ArrayList<String> array) {
+        JSONArray jArray = new JSONArray(array);
+        editor.remove(key);
+        editor.putString(key, jArray.toString());
+        editor.commit();
+    }
+    
+    /**
+     * Loads a JSONArray from shared preferences
+     * and converts it to an ArrayList<String>
+     * @param String key Preference key for SharedPreferences
+     * @return ArrayList<String> containing the saved values from the JSONArray
+     */
+    public ArrayList<String> getArray(String key) {
+        ArrayList<String> array = new ArrayList<String>();
+        String jArrayString = prefs.getString(key, "NOPREFSAVED");
+        if (jArrayString.matches("NOPREFSAVED")) return getDefaultArray();
+        else {
+            try {
+                JSONArray jArray = new JSONArray(jArrayString);
+                for (int i = 0; i < jArray.length(); i++) {
+                    array.add(jArray.getString(i));
+                }
+                return array;
+            } catch (JSONException e) {
+                return getDefaultArray();
+            }
+        }   
+    }
+}
 
 //############################################################################
+
+// shared preferences
+   private SharedPreferences preferences;
+   private SharedPreferences.Editor nsuserdefaults;
+
+// setup persistent data
+    preferences = this.getSharedPreferences("MyPreferences", MainActivity.MODE_PRIVATE);
+    nsuserdefaults = preferences.edit();
+
+    arrayOfMemberUrlsUserIsFollowing = new ArrayList<String>();
+    //Retrieve followers from sharedPreferences
+    Set<String> set = preferences.getStringSet("following", null);
+
+    if (set == null) {
+        // lazy instantiate array
+        arrayOfMemberUrlsUserIsFollowing = new ArrayList<String>();
+    } else {
+        // there is data from previous run
+        arrayOfMemberUrlsUserIsFollowing = new ArrayList<>(set);
+    }
+
+// convert arraylist to set, and save arrayOfMemberUrlsUserIsFollowing to nsuserdefaults
+    Set<String> set = new HashSet<String>();
+    set.addAll(arrayOfMemberUrlsUserIsFollowing);
+    nsuserdefaults.putStringSet("following", set);
+    nsuserdefaults.commit();
+
+
+//############################################################################
+
+1.Variable declaration i.e
+
+  SharedPreferences shared;
+  ArrayList<String> arrPackage;
+2.Variable initialization i.e
+
+ shared = getSharedPreferences("App_settings", MODE_PRIVATE);
+ // add values for your ArrayList any where...
+ arrPackage = new ArrayList<>();
+3.Store value to sharedPreference using packagesharedPreferences():
+
+ private void packagesharedPreferences() {
+   SharedPreferences.Editor editor = shared.edit();
+   Set<String> set = new HashSet<String>();
+   set.addAll(arrPackage);
+   editor.putStringSet("DATE_LIST", set);
+   editor.apply();
+   Log.d("storesharedPreferences",""+set);
+ }
+4.Retrive value of sharedPreference using retriveSharedValue():
+
+ private void retriveSharedValue() {
+   Set<String> set = shared.getStringSet("DATE_LIST", null);
+   arrPackage.addAll(set);
+   Log.d("retrivesharedPreferences",""+set);
+ }
+
+//############################################################################
+
+//Retrieve the values
+Set<String> set = myScores.getStringSet("key", null);
+
+//Set the values
+Set<String> set = new HashSet<String>();
+set.addAll(listOfExistingScores);
+scoreEditor.putStringSet("key", set);
+scoreEditor.commit();
+
+public void addTask(Task t) {
+  if (null == currentTasks) {
+    currentTasks = new ArrayList<task>();
+  }
+  currentTasks.add(t);
+
+  // save the task list to preference
+  SharedPreferences prefs = getSharedPreferences(SHARED_PREFS_FILE, Context.MODE_PRIVATE);
+  Editor editor = prefs.edit();
+  try {
+    editor.putString(TASKS, ObjectSerializer.serialize(currentTasks));
+  } catch (IOException e) {
+    e.printStackTrace();
+  }
+  editor.commit();
+}
+
+public void onCreate() {
+  super.onCreate();
+  if (null == currentTasks) {
+    currentTasks = new ArrayList<task>();
+  }
+
+  // load tasks from preference
+  SharedPreferences prefs = getSharedPreferences(SHARED_PREFS_FILE, Context.MODE_PRIVATE);
+
+  try {
+    currentTasks = (ArrayList<task>) ObjectSerializer.deserialize(prefs.getString(TASKS, ObjectSerializer.serialize(new ArrayList<task>())));
+  } catch (IOException e) {
+    e.printStackTrace();
+  } catch (ClassNotFoundException e) {
+    e.printStackTrace();
+  }
+}
